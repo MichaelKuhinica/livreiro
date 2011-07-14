4810106f46e3ff83a213dbba8bec0b475739b028
Michael Kuhinica <michael@webgoal.com.br>
2011-07-14 11:11:01 -0300

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Livreiro::Application.config.secret_token = '74b02fe6af8eee31e71d730bccc2d39932c5523145fb397bf297bfa33badb82572cf59272a58e2cd2af07e413a7696cf9d56bd9327d60690767ca906843c5f7b'
